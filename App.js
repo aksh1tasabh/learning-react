bc444a5f39ea130c9a20dc253fc08c0a09e0aad6
Akshita Sabharwal <akshita.sabharwal.2003@gmail.com>
2025-06-21 01:22:15 +0530

    Foundation changes

--- App.js
+++ App.js
@@ -1,6 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-const heading = React.createElement("h1", {id: "heading"}, "Hello World -- !!");
+const Title = () => (
+    <h1>This is a silly title</h1>
+);
+
+const HeaderComponent = () => (
+    <div id="container" className="container">
+        {Title()}
+        <Title />
+        <Title></Title>
+        <p>Useless para</p>
+    </div>
+);
+
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading);
+root.render(<HeaderComponent />);
